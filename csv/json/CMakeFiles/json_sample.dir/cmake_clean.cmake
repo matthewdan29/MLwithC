8e517ec85f550910ed2a5549f730c1b00224fd5e
Matthew K Daniels <140857904+matthewdan29@users.noreply.github.com>
2023-12-29 19:06:36 -0600

    Add files via upload
    This is part 2 also I have to upload the OpenCV application with the correct CMakeFile.txt the path need to be placed in right. Also Looking forward to DNN

---
+++ csv/json/CMakeFiles/json_sample.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/json_sample.dir/json.cpp.o"
+  "CMakeFiles/json_sample.dir/json.cpp.o.d"
+  "CMakeFiles/json_sample.dir/reviewsreader.cpp.o"
+  "CMakeFiles/json_sample.dir/reviewsreader.cpp.o.d"
+  "json_sample"
+  "json_sample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/json_sample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ csv/json/cpp/review.h
@@ -0,0 +1,18 @@
+#ifndef REVIEW_H
+#define REVIEW_H
+
+#include <string>
+
+struct Review
+{
+	std::string confidence; 
+	std::string evaluation; 
+	uint32_t id{0}; 
+	std::string language; 
+	std::string orentation; 
+	std::string remarks; 
+	std::string text; 
+	std::string timespan; 
+}; 
+
+#endif	/*REVIEW_H*/
