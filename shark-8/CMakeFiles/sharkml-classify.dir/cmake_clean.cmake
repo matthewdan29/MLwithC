00cb48298a44721090d5ade1c1cb004a88b5cf9c
Matthew K Daniels <140857904+matthewdan29@users.noreply.github.com>
2024-01-23 17:56:04 -0600

    Add files via upload

---
+++ shark-8/CMakeFiles/sharkml-classify.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sharkml-classify.dir/sharkml-classify.cpp.o"
+  "CMakeFiles/sharkml-classify.dir/sharkml-classify.cpp.o.d"
+  "sharkml-classify"
+  "sharkml-classify.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sharkml-classify.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
