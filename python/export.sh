b721eb2e996765b5121f58ebecff36723302ca09
Matthew K Daniels <140857904+matthewdan29@users.noreply.github.com>
2024-01-29 17:08:45 -0600

    Add files via upload

---
+++ python/export.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+#hey check the link i might forget and upload the wrong one also it might be a troll so just check the link before
+
+wget https://raw.githubusercontent.com/onnx/models/master/vision/classification/synset.txt
+
+wget 
+#python version should be less then 3.8 i.e. 3.8 or 3/7 because pytorch1.2 doesnt support python 3.8>
